a39d04a770b6046bafc06ca111f9f925af07d49e
rajaupadhyay <upadhyayaraja@gmail.com>
2017-03-20 22:11:17 +0000

    Regression with Features and labels

--- Regression.py
+++ Regression.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import quandl
+import math
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -10,6 +11,18 @@ df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Low'] * 100.0
 df['PCT_CHANGE'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_CHANGE', 'Adj. Volume']]
+
+# Adjusted close should be a feature
+
+forecast_col = "Adj. Close"
+df.fillna(-9999, inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df))) # Initial prediction out 10%
+
+# Label sorted out
+df['label'] = df[forecast_col].shift(-forecast_out) #Shift columns
+
 print(df.head())
 
+# train, test, predict
 
